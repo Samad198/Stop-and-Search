7c452c5ca71da9dfb5ec12e71de93c41b7112cd2
Samad <eeysaad@nottingham.ac.uk>
2021-05-03 11:21:10 +0100

    push without characters

--- Stop and Search/Assets/NPCController.cs
+++ Stop and Search/Assets/NPCController.cs
@@ -16,21 +16,33 @@ public class NPCController : MonoBehaviour
     public bool isFocused;
     private Vector3 currentDirection;
     Vector3 rotation;
+    public int personId;
+    public string my_age_range,my_gender, my_race, my_clothes_top,my_clothes_bottom, my_build,my_shoes,my_hair;
+    public class PD {
+    public string age_range,gender, race, clothes_top,clothes_bottom, build,shoes,hair, description;
     
-    
-
-    public string my_age_range,my_gender, my_race, my_clothes_top,my_clothes_bottom, my_build;
-
+    public PD(string age_range,string gender,string race,string clothes_top,string clothes_bottom,string build,string shoes,string hair,string description) {
+        this.age_range = age_range;
+        this.gender = gender;
+        this.race = race;
+        this.clothes_top = clothes_top;
+        this.clothes_bottom = clothes_bottom;
+        this.build = build;
+        this.shoes = shoes;
+        this.hair = hair;
+        this.description = description;
+    }
+}
     
 
 
     void OnCollisionEnter(Collision col){
-   
    ChangeDirection();
  }
  
-     // Use this for initialization
 
+
+     // Use this for initialization
      private void Awake(){
          animator = GetComponentInChildren<Animator>();
          
@@ -41,6 +53,109 @@ public class NPCController : MonoBehaviour
          ChangeDirection();
          canvasGroup.alpha = 0f;
          collider.alpha = 0f;
+            PD pd = null;
+         switch(personId){
+             case 1:
+                pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 2:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 3:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 4:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 5:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 6:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 7:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 8:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 9:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 10:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 11:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 12:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 13:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 14:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 15:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 16:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 17:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 18:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 19:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 20:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 21:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 22:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 23:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 24:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 25:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 26:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 27:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 28:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             case 29:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             default:
+             pd = new PD("25-30","male","IC1","grey top","grey shorts","slim","white trainers","blonde hair","A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers");
+             break;
+             
+         }
+
+         my_age_range = pd.age_range;
+         my_gender = pd.gender;
+         my_race = pd.race;
+         my_clothes_top = pd.clothes_top;
+         my_clothes_bottom =pd.clothes_bottom;
+         my_build = pd.build;
+         my_shoes = pd.shoes;
+         my_hair = pd.hair;
         
      }
      
@@ -106,6 +221,8 @@ public class NPCController : MonoBehaviour
     if(MatchingDescriptionsData.currentPersonDescriptor.clothes_top != "" && my_clothes_top != MatchingDescriptionsData.currentPersonDescriptor.clothes_top  ){MatchingDescriptionsData.score = MatchingDescriptionsData.score - 1;}
     if(MatchingDescriptionsData.currentPersonDescriptor.clothes_bottom != "" && my_clothes_bottom != MatchingDescriptionsData.currentPersonDescriptor.clothes_bottom  ){MatchingDescriptionsData.score = MatchingDescriptionsData.score - 1;}
     if(MatchingDescriptionsData.currentPersonDescriptor.build != "" && my_build != MatchingDescriptionsData.currentPersonDescriptor.build ){MatchingDescriptionsData.score = MatchingDescriptionsData.score - 1;}
+    if(MatchingDescriptionsData.currentPersonDescriptor.shoes != "" && my_shoes != MatchingDescriptionsData.currentPersonDescriptor.shoes ){MatchingDescriptionsData.score = MatchingDescriptionsData.score - 1;}
+    if(MatchingDescriptionsData.currentPersonDescriptor.hair != "" && my_hair != MatchingDescriptionsData.currentPersonDescriptor.hair ){MatchingDescriptionsData.score = MatchingDescriptionsData.score - 1;}
     MatchingDescriptionsData.testsLeft -= 1;
 
     if(MatchingDescriptionsData.testsLeft <= 0){
--- Stop and Search/Assets/mdData.cs
+++ Stop and Search/Assets/mdData.cs
@@ -5,16 +5,18 @@ public static class MatchingDescriptionsData
 
 
 public class PersonDescriptor {
-    public string age_range,gender, race, clothes_top,clothes_bottom, build, description;
+    public string age_range,gender, race, clothes_top,clothes_bottom, build,shoes,hair, description;
     
-    public PersonDescriptor(string age_range,string gender,string race,string clothes_top,string clothes_bottom,string build,string description) {
+    public PersonDescriptor(string age_range,string gender,string race,string clothes_top,string clothes_bottom,string build,string shoes,string hair,string description) {
         this.age_range = age_range;
         this.gender = gender;
         this.race = race;
         this.clothes_top = clothes_top;
         this.clothes_bottom = clothes_bottom;
         this.build = build;
+        this.shoes = shoes;
         this.description = description;
+        this.hair = hair;
     }
 
     public PersonDescriptor() {
@@ -24,7 +26,9 @@ public class PersonDescriptor {
         this.clothes_top = "";
         this.clothes_bottom = "";
         this.build = "";
-        this.description = "it was a rando";
+        this.description = "A 25-30 year old IC1 slim male with blonde hair wearing a grey top, grey shorts and white trainers.";
+        this.shoes = "";
+        this.hair = "";
     }
 }
 
